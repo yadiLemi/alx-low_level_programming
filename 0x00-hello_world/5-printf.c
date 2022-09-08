8a2446a545698255858a7a58ed010ff1115442cf
yadiLemi <yadetaabera5@gmail.com>
2022-09-08 03:34:42 -0700

    updated 5th task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *   
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	        return (0);
-		 }
+	return (0);
+}
