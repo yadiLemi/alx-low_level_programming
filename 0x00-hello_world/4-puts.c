ae55ad4165d1bcc1d4195672ee0bd046064782a4
yadiLemi <yadetaabera5@gmail.com>
2022-09-08 04:53:48 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Prints "Programming is like building a multilingual
-                  puzzle, followed by a new line.
+ *                 puzzle, followed by a new line.
  * 
  * Return: Always 0.
  */
