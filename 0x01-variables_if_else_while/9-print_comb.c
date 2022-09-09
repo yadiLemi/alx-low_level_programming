5b3f7f1b97936e2274d103e73b1ca6a533ae3012
yadiLemi <yadetaabera5@gmail.com>
2022-09-09 14:44:48 -0700

    9th task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print the case
+ * 
+ * description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+putchar(c);
+if (c != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return(0);
