c717b5fd1461b4c820faf547bbfa01018c2d3270
yadiLemi <yadetaabera5@gmail.com>
2022-09-09 13:46:52 -0700

    edited

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  *main - print the case
- * 
+ *
  * description: using the main function
  * Return: 0
  */
@@ -11,7 +11,7 @@ char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 if (ch != 'e' && ch != 'q')
-{	
+{
 putchar(ch);
 }
 }
