0ec400642bfe91f8aa06cf666e3f9d570d2ecf83
yadiLemi <yadetaabera5@gmail.com>
2022-09-10 01:49:14 -0700

    11th tsak

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - print the case
+ * 
+ * description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+int n = 0, n2, n3;
+while (n1 <= 9)
+{
+n2 = 0;
+while (n2 <= 9)
+{
+n3 = 0;
+while (n3 <= 9)
+{
+if (n1 != n2 && n1 < n2 && n2 != n3 && n2 < n3)
+{
+putchar(n1 + 48);
+putchar(n2 + 48);
+putchar(n3 + 48);
+if (n1 + n2 + n3 !=24)
+{
+putchar(',');
+putchat(' ');
+}
+}
+++n3;
+}
+++n2;
+}
+++n1;
+}
+putchar('\n');
+return (0);
+}
