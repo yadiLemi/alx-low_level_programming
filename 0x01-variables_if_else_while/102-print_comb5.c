2778b9271051985645d20081f73ad23a800e90d1
yadiLemi <yadetaabera5@gmail.com>
2022-09-10 02:14:46 -0700

    last task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+/**
+ *main - print the case
+ *
+ * description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+int n1 = 0, n2;
+while (n1 <= 99)
+{
+if (n2 != n1)
+{
+putchar((n1 / 10 ) + 48);
+putchar((n1 % 10 ) + 48);
+putchar(' ');
+putchar((n2 / 10 ) + 48);
+putchar((n2 % 10 ) + 48);
+if (n1 != 98 || n2 != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+++n2;
+}
+++n1;
+}
+putchar('\n');
+return (0);
+}
