874ea728e14aeddc1d52afa7780c9b5a11b8cfa7
yadiLemi <yadetaabera5@gmail.com>
2022-09-16 06:13:25 -0700

    3rd

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,12 @@
 
 /**
  * print_numbers - Print numbers between 0 to 9 incl.
- * 
+ *
  * Return: Void.
  */
 void print_numbers(void)
 {
 int i;
-
 for (i = 0; i < 10; i++)
 {
 _putchar(i + '0');
