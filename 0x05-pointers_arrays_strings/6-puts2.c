2ce0bb10a996b9467a75d93ab2f5317726b86c23
yadiLemi <yadetaabera5@gmail.com>
2022-09-19 06:12:04 -0700

    task 6-9

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: type of character
+ */
+void puts2(char *str)
+{
+int i = 0;
+int j = 0;
+
+while (str[i] != '\0')
+{
+i++;
+}
+
+while (j < i)
+{
+_putchar(str[j]);
+j = j + 2;
+}
+_putchar('\n');
+}
